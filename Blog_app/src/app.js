37512312d14d6108ff65a051c0ed5d871986a5d5
Rugved789 <rugved.gadge.789@gmail.com>
2026-02-25 23:53:23 +0530

    backend_complete

--- Blog_app/src/app.js
+++ Blog_app/src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import multer from "multer";
 import uploadfile from "./services/storage.service.js";
+import { blogModel } from "./models/blog.model.js";
 
 
 export const app = express();
@@ -13,8 +14,23 @@ app.post("/create-blog",upload.single("image"), async (req,res)=>{
     console.log(req.file)
 
     const result = await uploadfile(req.file.buffer);
-    console.log(result);
+    
+   const blog = await blogModel.create({
+        img_url:result.url,
+        caption:req.body.caption,
+    })
+
+    return res.status(201).json({
+        message:"blog Created",
+        blog
+    })
+
 })
 app.get("/get-blog",async (req,res)=>{
-    
+    const userBlog = await blogModel.find();
+
+    return res.status(200).json({
+        message:"Blog got sucessfully",
+        userBlog
+    })
 })
