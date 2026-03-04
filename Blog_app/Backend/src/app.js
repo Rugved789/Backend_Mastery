a0c329377a4ed7d2c665810b12a551ae4f120369
Rugved789 <rugved.gadge.789@gmail.com>
2026-03-04 21:27:18 +0530

    cookie steup

--- Blog_app/Backend/src/app.js
+++ Blog_app/Backend/src/app.js
@@ -4,12 +4,15 @@ import uploadfile from "./services/storage.service.js";
 import { blogModel } from "./models/blog.model.js";
 import cors from "cors";
 import authRoutes from "./routes/auth.route.js";
+import cookieParser from "cookie-parser";
 
 export const app = express();
 // enable CORS middleware (remember the function call)
 app.use(cors());
 app.use(express.json());
+app.use(cookieParser());
 app.use("/api/auth", authRoutes);
+
 const upload = multer({ storage: multer.memoryStorage() });
 
 app.post("/create-blog", upload.single("image"), async (req, res) => {
--- Blog_app/Backend/src/routes/auth.route.js
+++ Blog_app/Backend/src/routes/auth.route.js
@@ -4,6 +4,11 @@ import authController from "../controllers/auth.controller.js";
 const router = express.Router();
 
 router.post("/register", authController.registerUser);
+router.get("/test", (req, res) => {
+  res.json({
+    message: "Test reoute",
+    cookies: req.cookies,
+  });
+});
 
 export default router;
-
