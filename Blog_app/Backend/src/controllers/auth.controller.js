0b9d905c26be373b37cf0ee210372dc26dbff743
Rugved789 <rugved.gadge.789@gmail.com>
2026-03-04 19:59:26 +0530

    auth completion

--- Blog_app/Backend/src/controllers/auth.controller.js
+++ Blog_app/Backend/src/controllers/auth.controller.js
@@ -1,4 +1,7 @@
 import { userModel } from "../models/user.model.js";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+dotenv.config();
 
 
 async function registerUser(req,res) {
@@ -6,6 +9,16 @@ async function registerUser(req,res) {
 
     const user = await userModel.create({
         userName,email,password
+    })
+
+    const token = jwt.sign({
+        id:user._id,
+    },process.env.JWT_SECRET);
+
+    res.status(201).json({
+        message:"User Created Sucessfully",
+        user,
+        token
     })
 }
 
